8b1a62488124c6b01970828619465d3c4d1a809d
Ulug'bek <ulugbekgozixonov@gmail.com>
2022-08-29 12:50:59 +0500

    3.0.2 version

---
+++ src/main/java/uz/jl/temp/modul/Test.java
@@ -0,0 +1,15 @@
+package uz.jl.temp.modul;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Test {
+
+    public int TwoSumNumber(int a,int b){
+        return a+b;
+    }
+}
