e702d15395d504829ecfe62d579e51a26c90c099
Ulug'bek <ulugbekgozixonov@gmail.com>
2022-08-29 13:44:04 +0500

    University added

---
+++ src/main/java/uz/jl/temp/modul/University.java
@@ -0,0 +1,13 @@
+package uz.jl.temp.modul;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class University {
+    private String name;
+    private Student student;
+}
